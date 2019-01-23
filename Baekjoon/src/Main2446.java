ae9b8a2237b689a1b35f3455abbbb8ebebcb0c57
puhehe99 <puhehe99@gmail.com>
2019-01-23 17:04:42 +0900

    0123

---
+++ Baekjoon/src/Main2446.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main2446 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(printStar(N));
+	}
+
+	private static String printStar(int n) {
+		
+		
+		
+		return null;
+	}
+
+}
