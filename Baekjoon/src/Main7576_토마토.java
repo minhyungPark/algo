917fb02c45e1e23dbc8450af5afea696ecf718ab
puhehe99 <puhehe99@gmail.com>
2019-01-25 17:59:46 +0900

    0125

---
+++ Baekjoon/src/Main7576_토마토.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main7576_토마토 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
+		
+		
+	}
+
+}
