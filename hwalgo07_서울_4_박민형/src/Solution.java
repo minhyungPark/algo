4075ae80999b0f22297cec8d736c1862ddb2b369
박민형 <puhehe99@gmail.com>
2019-01-29 01:16:11 +0900

    0128hw

--- hwalgo07_서울_4_박민형/src/Solution.java
+++ hwalgo07_서울_4_박민형/src/Solution.java
@@ -1,15 +1,14 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-
 	private static class Node{
 		int data;
 		Node next;
 
-		Node(){	};
 		Node(int data){
 			this.data = data;
 		}
@@ -20,12 +19,7 @@ public class Solution {
 	}
 	private static class Linked{
 		Node head;
-		Node next;
-		int size;
-		public Linked() {
-			this.size=0;
-		}
-		
+		public Linked() {}
 		public Node getLastNode() {
 			Node currNode = head;
 			if(currNode != null) {
@@ -36,27 +30,6 @@ public class Solution {
 			}
 			return null;
 		}
-		
-		//특정노드의 바로 이전 노드 찾기
-		public Node getPreviousNode(Node node) {
-			Node currNode = head;
-			Node preNode = null;
-			Node nextNode;
-			if(currNode != null) {
-				while((nextNode = currNode.next)!=null) {
-					if(nextNode == node) {
-//						return currNode;
-						preNode = currNode;
-						break;
-					}
-					currNode = nextNode;
-				}
-			}
-			return preNode;
-//			return null;
-		}
-		
-		// 마지막 노드로 추가
 		public void addLastNode(int data) {
 			Node lastNode = getLastNode();
 			Node newNode = new Node(data);
@@ -67,90 +40,60 @@ public class Solution {
 				lastNode.next = newNode;
 			}
 		}
-		
-		// 첫번째 노드로 추가
 		public void addFirstNode(int data) {
 			Node newNode = new Node(data,head);
 			head = newNode;
 		}
-		
-		// 지정한 뒤에 추가
 		public void insertAfterNode(int index,int data) {
+			if(index == 0) {
+				addFirstNode(data);
+				return;
+			}
 			Node currNode = head;
-			int i=0;
-			while(i>index) {
+			Node newNode = new Node(data);
+			int i=1;
+			while(i<index) {
 				currNode = currNode.next;
 				++i;
 			}
-			Node newNode = new Node(data);
 			Node temp = currNode.next;
 			currNode.next = newNode;
 			newNode.next = temp;
 		}
-		
-		// 원하는 노드 찾기
-		public Node getNode(int data) {
-			Node currNode = head;
-			Node node=null;
-			while(currNode != null){
-				if(currNode.data == data) {
-					node  = currNode;
-					break;
-				}
-				currNode = currNode.next;
-			}
-			return node;
-		}
-		
-		// 마지막 노드 삭제
-		public void deleteLastNode() {
-			Node lastNode = getLastNode();
-			if(lastNode == null)return;
-			Node preNode = getPreviousNode(lastNode);
-			if(preNode != null) {		// 이전 노드가 있다면
-				preNode.next = null;
-			}else {						// 이전 노드가 없다면 (즉, 1개짜리 list라면)
-				head = null;
-			}
-		}
 		public void printList() {
 			Node currNode = head;
-			System.out.print("LinkedList = [ ");
-			while(currNode != null) {
+			for(int i=0;i<10;++i) {
 				System.out.print(currNode.data+" ");
 				currNode = currNode.next;
 			}
-			System.out.println("]");
+			System.out.println();
 		}
 		
 	}
 	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("input.txt"));
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		
 		for(int t=1;t<=10;++t) {
-			
-		}
-		Linked list = new Linked();
-		int N = Integer.parseInt(bf.readLine());
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		for(int n=0;n<N;++n) {
-			list.addLastNode(Integer.parseInt(st.nextToken()));
-		}
-		list.printList();
-		N = Integer.parseInt(bf.readLine());
-		for(int n=0;n<N;++n) {
+			Linked list = new Linked();
+			int N = Integer.parseInt(bf.readLine());
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			for(int n=0;n<N;++n) {
+				list.addLastNode(Integer.parseInt(st.nextToken()));
+			}
+			N = Integer.parseInt(bf.readLine());
 			String s = bf.readLine();
 			st = new StringTokenizer(s);
-			s = st.nextToken();
-			int index = Integer.parseInt(st.nextToken());
-			int count = Integer.parseInt(st.nextToken());
-			for(int i=0;i<count;++i) {
-				list.insertAfterNode(index, Integer.parseInt(st.nextToken()));
-				index = index+2;
+			for(int n=0;n<N;++n) {
+				s = st.nextToken();
+				int index = Integer.parseInt(st.nextToken());
+				int count = Integer.parseInt(st.nextToken());
+				for(int i=0;i<count;++i) {
+					list.insertAfterNode(index++, Integer.parseInt(st.nextToken()));
+				}
 			}
+			System.out.print("#"+t+" ");
+			list.printList();
 		}
-		list.printList();
-		
 	}
-
 }
