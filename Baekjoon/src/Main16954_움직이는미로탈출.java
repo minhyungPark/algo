1b99985371a24dd40e7bb8e4a40c980c08d550d9
minhyungPark <puhehe99@gmail.com>
2019-08-15 21:47:11 +0900

    백준 움직이는미로탈출

--- Baekjoon/src/Main16954_움직이는미로탈출.java
+++ Baekjoon/src/Main16954_움직이는미로탈출.java
@@ -4,61 +4,62 @@ import java.util.Scanner;
 
 public class Main16954_움직이는미로탈출 {
 
-	static int N=8;
-	static int[][][] map;
-	static int[] dx = {0,0,-1,1};
-	static int[] dy = {1,-1,0,0};
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		map = new int[10][8][8];
-		for(int i=0;i<8;++i) {
-			char[] temp = sc.next().toCharArray();
-			for(int j=0;j<8;++j) {
-				if(temp[j]=='#')map[0][i][j] = 1;
-			}
-		}
-		makeMap();
-		int result = bfs();
-		System.out.println(result);
-		
-		
-	}
-	private static int bfs() {
-		boolean[][][] visited = new boolean[9][8][8];
-		Queue<int[]> que = new LinkedList<int[]>();
-		int[] temp = {7,0,0};
-		que.add(temp);
-		visited[0][7][0] = true;
-		while(!que.isEmpty()) {
-			int[] c = que.poll();
-			if(c[0]==0&&c[1]==7)return 1;
-			int vv = c[2]>7?8:c[2];
-			for(int i=0;i<4;++i) {
-				int nx = c[0]+dx[i];
-				int ny = c[1]+dy[i];
-				if(nx<0||ny<0||nx>=N||ny>=N)continue;
-				if(visited[vv][nx][ny]) continue;
-				if(map[vv][nx][ny]==1||map[vv+1][nx][ny]==1)continue;
-				visited[vv][nx][ny] = true;
-				int[] t = {nx,ny,c[2]+1};
-				que.add(t);
-			}
-		}
-		
-		return 0;
-	}
-	private static void makeMap() {
-		for(int i=0;i<7;++i) {
-			clone(map[i],map[i+1]);
-		}
-	}
-	private static void clone(int[][] arr,int[][] target) {
-		for(int i=1;i<8;++i) {
-			for(int j=0;j<8;++j) {
-				target[i][j]=arr[i-1][j];
-			}
-		}
-		
-	}
+    static int N=8,M=8;
+    static int[] dx = {0,0,-1,1,1,1,-1,-1,0};
+    static int[] dy = {1,-1,0,0,1,-1,1,-1,0};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[N][M];
+        for(int i=0;i<N;++i){
+            char[] tChar = sc.next().toCharArray();
+            for(int j=0;j<M;++j){
+                if(tChar[j]=='#')arr[i][j] = -1;
+            }
+        }
+        int res = bfs(arr);
+        System.out.println(res);
+    }
+
+    private static int bfs(int[][] arr) {
+        Queue<int[]> que = new LinkedList<>();
+        boolean[][][] check = new boolean[N][M][64];
+        int[] tt= {7,0,0};
+        check[7][0][0] = true;
+        que.offer(tt);
+        while(!que.isEmpty()){
+
+            int qs = que.size();
+            for(int i=0;i<qs;++i){
+                int[] c =  que.poll();
+                int x = c[0];
+                int y = c[1];
+                int t = c[2];
+                if(x==0||y==7)return 1;
+                if(arr[x][y]==-1)continue;
+                for(int d = 0;d<9;++d){
+                    int nx = x+dx[d];
+                    int ny = y+dy[d];
+                    if(nx<0||ny<0||nx>=N||ny>=M)continue;
+                    if(arr[nx][ny]==-1||check[nx][ny][t+1])continue;
+                    int[] temp = {nx,ny,t+1};
+                    que.offer(temp);
+                    check[nx][ny][t+1] = true;
+                }
+            }
+            downArr(arr);
+        }
+
+        return 0;
+    }
+
+    private static void downArr(int[][] arr) {
+        for(int j=0;j<M;++j){
+            for(int i=7;i>0;--i){
+                arr[i][j] = arr[i-1][j];
+            }
+            arr[0][j] = 0;
+        }
+    }
+
 
 }
